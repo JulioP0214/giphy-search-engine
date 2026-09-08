7a9f03b82f02526376f86ce9faa1d484934ca974
Julio <jpandruby1@yahoo.comm>
2026-09-08 11:54:17 -0500

    connects giphy api

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const searchForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#search-input");
+const gifResults = document.querySelector("#gif-results");
+
+const apiKey = "phWzOa8leaj2uUeRJjEbj2rHkib8DVdm";
 
 searchForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const searchTerm = searchInput.value;
 
-  console.log(searchTerm);
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=12`;
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 });
